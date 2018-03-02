001aaa7d013aa972af25b543ba6c5c2c38ca72ea
Liang Yu <liangjyu@gmail.com>
2018-03-02 14:33:51 -0800

    added recognition

--- py_faces/face_recognition.py
+++ py_faces/face_recognition.py
@@ -1,25 +1,44 @@
 import os
 import cv2
+import numpy as np
 from face_detect import FaceDetect
 
 
 class face_recognition():
     def __init__(self):
         self.face_detector = FaceDetect()
+        self.face_recognizer = cv2.face.LBPHFaceRecognizer_create()
+        # TODO choose a recognizer
+        # or use EigenFaceRecognizer by replacing above line with
+        # face_recognizer = cv2.face.EigenFaceRecognizer_create()
+        # or use FisherFaceRecognizer by replacing above line with
+        # face_recognizer = cv2.face.FisherFaceRecognizer_create()
+        self.faces = []
+        self.labels = []
+        self.label_dict = {}
 
-    def train(self, path):
+    def load_training_data(self, path):
         if os.path.isdir(path):
-            faces = []
-            labels = []
+            n_trainees = 1
             for training_dir in os.listdir(path):
                 label = os.path.basename(training_dir)
-                print(label)
+                self.label_dict[n_trainees] = label
+                # TODO parallize this for speed up later
                 for f in os.listdir(path+'/'+training_dir):
-                    print('\t',f)
+                    # build correct path
                     img_path = '/'.join([path,training_dir,f])
+                    # detect face; assume only 1 face and it the 1st one
                     bbox, face = self.face_detector.in_file(img_path)
-                    # assume only 1 face and it the 1st one
-                    faces.append(face[0])
-                    labels.append(label)
-        else:
-            return
+                    # append respective list
+                    self.faces.append(face[0])
+                    self.labels.append(n_trainees)
+                n_trainees += 1
+
+    def train(self, path):
+        # load training data if none
+        if not self.faces and not self.labels:
+            self.load_training_data(path)
+
+        # train
+        if self.faces and self.labels and len(self.faces) == len(self.labels):
+            self.face_recognizer.train(self.faces, np.array(self.labels))
