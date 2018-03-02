1267ba1755946c74e269795d4197fba47d3b3fe3
Liang Yu <liangjyu@gmail.com>
2018-03-02 15:48:28 -0800

    added made faces uniformly sized
    attempt to fix Beyonce being overwhelmingly chosen without; now the Elvis always chosen

--- py_faces/face_recognition.py
+++ py_faces/face_recognition.py
@@ -16,6 +16,7 @@ class face_recognition():
         self.faces = []
         self.labels = []
         self.label_dict = {}
+        self.avg_face_dim = ()
 
     def load_training_data(self, path):
         if os.path.isdir(path):
@@ -34,17 +35,31 @@ class face_recognition():
                     self.labels.append(n_trainees)
                 n_trainees += 1
 
+    def uniform_faces(self):
+        # collect dimensions of all faces
+        face_dims = [f.shape for f in self.faces]
+        # get average value
+        self.avg_face_dim = tuple(np.mean(face_dims, axis=0).astype(int))
+        # resize all to be the same size
+        self.faces = [cv2.resize(face, self.avg_face_dim) for face in self.faces]
+
     def train(self, path):
         # load training data if none
         if not self.faces and not self.labels:
             self.load_training_data(path)
+            self.uniform_faces()
 
         # train
         if self.faces and self.labels and len(self.faces) == len(self.labels):
             self.face_recognizer.train(self.faces, np.array(self.labels))
 
     def predict_img(self, img):
+        # find face(s)
         bbox, faces = self.face_detector.in_frame(img)
+        # make face same size as training data
+        if self.avg_face_dim:
+            faces[0] = cv2.resize(faces[0], self.avg_face_dim)
+        # predict
         label, confidence = self.face_recognizer.predict(faces[0])
         return label, confidence
 
