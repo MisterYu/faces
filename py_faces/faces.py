e60c0cb358738a8e6fa374e15e89a5c7f0601df3
Liang Yu <liangjyu@gmail.com>
2018-03-03 14:55:47 -0800

    renamed and commented for clarity

--- py_faces/faces.py
+++ py_faces/faces.py
@@ -12,15 +12,19 @@ class faces(QtWidgets.QMainWindow, Ui_MainWindow):
         super(faces, self).__init__()
         self.setupUi(self)
 
+        # init objects that process faces
         self.face_detector = FaceDetect()
-        self.face_recognizer = FaceRecognition()
+        self.face_recognizer = FaceRecognition()    # this one is SLOOOOwwww
         self.face_recognizer.train('../face_data/')
 
+        # init cameras
         self.camera = cv_camera()
+
+        # init and connect controls
         self.start_pushButton.clicked.connect(self.start)
         self.camera_spinBox.setMaximum(self.camera.n_cameras-1)
         self.camera_spinBox.valueChanged.connect(self.change_camera)
-        self.info_label.setText('so fresh and so clean')
+        self.info_label.setText('ready to go!')
 
         self.face_detect = False
         self.face_detect_checkBox.stateChanged.connect(self.update_face_detect)
@@ -32,12 +36,10 @@ class faces(QtWidgets.QMainWindow, Ui_MainWindow):
         self.confidence_spinBox.valueChanged.connect(self.update_threshold)
 
     def start(self):
-        if not self.camera:
-            self.camera = cv_camera(0)
-            self.stop_pushButton.clicked.connect(self.stop)
+        self.stop_pushButton.clicked.connect(self.stop)
 
         self.timer = QtCore.QTimer()
-        self.timer.timeout.connect(self.get_camera_frame)
+        self.timer.timeout.connect(self.process_display_frame)
         self.timer.start(1000./self.camera.fps)
         self.info_label.setText('camera started')
 
@@ -62,23 +64,34 @@ class faces(QtWidgets.QMainWindow, Ui_MainWindow):
         self.timer.stop()
         self.info_label.setText('camera stopped')
 
-    def get_camera_frame(self):
+    def process_display_frame(self):
+        # pull a frame from chosen camera
         frame = self.camera.get_frame()
+
         if self.face_detect:
+            # do face detection
             bboxes, faces = self.face_detector.in_frame(frame)
+            # non-Beyonce color
             box_color = (204, 255, 0)
+            # update face status
             self.face_detect_label.setText('{0} faces found'.format(len(faces)))
+            # loop thru faces and draw boxes in frame
             for bbox, face in zip(bboxes, faces):
+                # do Beyonce reconition
                 if self.face_recognition:
                     label, confidence = self.face_recognizer.predict_face(face)
+                    # default to no Beyonce; it's not common after all...
                     str_recog = 'Beyonce not recognized'
+                    # if confidence of detection > threshold change text and box color
                     if confidence > self.b_threshold:
                         str_recog = 'Beyonce recognized @{0:5.4f} confidence'.format(confidence)
                         box_color = (209, 159, 232)
                     self.face_recognition_label.setText(str_recog)
+                # draw bounding boxes
                 x, y, w, h = bbox
                 cv2.rectangle(frame, (x, y), (x+w, y+h), box_color, 2)
 
+        # do Qt things to frame so it can be set in a QLabel
         img = QtGui.QImage(frame, frame.shape[1], frame.shape[0], QtGui.QImage.Format_RGB888)
         pix = QtGui.QPixmap.fromImage(img)
         self.video_frame_label.setPixmap(pix)
